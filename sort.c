4d7a53254d8d6ec662ceebb3f36124a5717d1f3a
tweety <ekak9380@gmail.com>
2018-11-01 16:28:46 +0900

    6-1 sort

---
+++ sort.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N_SORT 5
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int nums[N_SORT];
+	int i;
+	
+	printf("input %i numbers : ", N_SORT);
+	for (i=0;i<N_SORT;i++)
+	{
+		scanf("%d",&nums[i]);
+	}
+	
+	//sorting function
+	//bubbleSort(); 
+	
+	printf("sorted numbers are : ");
+	for(i=0;<N_SORT;i++)
+	{
+		printf("%d",nums[i]);
+	}
+	
+	return 0;
+}
